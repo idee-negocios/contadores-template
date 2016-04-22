2625d9321b99890bb5693b336efa392529db4ddf
Alan <kiriost.s@gmail.com>
2016-04-21 23:24:21 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Contadores</title>
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Signika' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Enriqueta' rel='stylesheet' type='text/css'>
+    <!-- Styles -->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/css/reset.scss" type="text/css" charset="utf-8" />
+    <link rel="stylesheet" href="/css/style.css" type="text/css" charset="utf-8" />
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/js/main.js"></script>
+</head>
+
+<body>
+
+    <div class="jumbotron">
+        <h1><a href="/">Contadores S.A.</a></h1>
+    </div>
+
+    <nav class="navbar navbar-default">
+        <ul class="nav navbar-nav">
+            <li><a href="#">INICIO</a></li>
+            <li><a href="#">QUIÉNES SOMOS</a></li>
+            <li><a href="#">ÁREA DE COMPETENCIA</a></li>
+            <li><a href="#">CONTACTO</a></li>
+        </ul>
+    </nav>
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 header-image">
+            <img src="/img/contadores.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3 header-image-bottom">
+            <img src="/img/liftedshadow_medium.png" />
+        </div>
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-4 section">
+                <h1>Especialistas en Impuestos</h1>
+                <img src="/img/especialistas_impuestos.jpg" />
+                <p>Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
+                <a href="#">MÁS</a>
+            </div>
+
+            <div class = "col-md-4 section">
+                <h1>Nuestro Equipo</h1>
+                <img src="/img/nuestro_equipo.jpg" />
+                <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
+                <a href="#">MÁS</a>
+            </div>
+
+            <div class="col-md-4 section">
+                <h1>Áreas de Competencia</h1>
+                <ul>
+                    <li>Impuestos</li>
+                    <li>Liquidaciones de sueldos</li>
+                    <li>Asesoría</li>
+                    <li>Testamentos y fideicomisos</li>
+                    <li>Software de contabilidad</li>
+                    <li>Teneduría de libros</li>
+                    <li>Selección de entidades</li>
+                    <li>Administración financiera</li>
+                </ul>
+                <a href="#">MÁS</a>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-4 post">
+                <h1>Noticias y Publicaciones</h1>
+                <img src="/img/post_1.jpg" />
+                <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
+                <a href="#">Más...</a>
+            </div>
+            <div class="col-md-4 post">
+                <img src="/img/post_2.jpg" />
+                <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
+                <a href="#">Más...</a>
+            </div>
+            <div class="col-md-3 post">
+                <h1>Testimonios</h1>
+                <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
+                <h3>Samuel Díaz</h3>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer">
+        <div class="contact">
+
+        </div>
+        <div class="follow">
+
+        </div>
+        <div class="member">
+
+        </div>
+    </div>
+</body>
+
+</html>
