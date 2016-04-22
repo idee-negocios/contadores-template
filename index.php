7f8ca6a7994589bc636bb67f1a023e7ffc3e40da
Alan <kiriost.s@gmail.com>
2016-04-22 04:36:46 -0300

    Add PHP-Slim and view templates
    View templates with Twig. I'm near to finish.

--- index.php
+++ index.php
@@ -1,116 +1,47 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-<head>
-    <meta charset="UTF-8">
-    <title>Contadores</title>
-    <!-- Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Signika' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Enriqueta' rel='stylesheet' type='text/css'>
-    <!-- Styles -->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="/css/reset.scss" type="text/css" charset="utf-8" />
-    <link rel="stylesheet" href="/css/style.css" type="text/css" charset="utf-8" />
-    <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="/js/main.js"></script>
-</head>
+require 'vendor/autoload.php';
 
-<body>
+$app = new \Slim\App;
 
-    <div class="jumbotron">
-        <h1><a href="/">Contadores S.A.</a></h1>
-    </div>
+$container = $app->getContainer();
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('templates', [
+        'cache' => false
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
 
-    <nav class="navbar navbar-default">
-        <ul class="nav navbar-nav">
-            <li><a href="#">INICIO</a></li>
-            <li><a href="#">QUIÉNES SOMOS</a></li>
-            <li><a href="#">ÁREA DE COMPETENCIA</a></li>
-            <li><a href="#">CONTACTO</a></li>
-        </ul>
-    </nav>
+    return $view;
+};
 
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2 header-image">
-            <img src="/img/contadores.jpg" />
-        </div>
-    </div>
+$app->get('/', function(Request $request, Response $response) {
+    return $this->view->render($response, 'home.html');
+});
 
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3 header-image-bottom">
-            <img src="/img/liftedshadow_medium.png" />
-        </div>
-    </div>
+$app->get('/quienes_somos', function(Request $request, Response $response) {
+    return $this->view->render($response, 'quienes_somos.html');
+});
 
-    <div class="container">
+$app->get('/area_competencia', function(Request $request, Response $response) {
+    return $this->view->render($response, 'area_competencia.html');
+});
 
-        <div class="row">
-            <div class="col-md-4 section">
-                <h1>Especialistas en Impuestos</h1>
-                <img src="/img/especialistas_impuestos.jpg" />
-                <p>Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
-                <a href="#">MÁS</a>
-            </div>
+$app->get('/contacto', function(Request $request, Response $response) {
+    return $this->view->render($response, 'contacto.html');
+});
 
-            <div class = "col-md-4 section">
-                <h1>Nuestro Equipo</h1>
-                <img src="/img/nuestro_equipo.jpg" />
-                <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
-                <a href="#">MÁS</a>
-            </div>
+// $app->get('/hello/{name}', function (Request $request, Response $response) {
+//     $name = $request->getAttribute('name');
+//     $response->getBody()->write("Hello, $name");
+//
+//     return $response;
+// });
 
-            <div class="col-md-4 section">
-                <h1>Áreas de Competencia</h1>
-                <ul>
-                    <li>Impuestos</li>
-                    <li>Liquidaciones de sueldos</li>
-                    <li>Asesoría</li>
-                    <li>Testamentos y fideicomisos</li>
-                    <li>Software de contabilidad</li>
-                    <li>Teneduría de libros</li>
-                    <li>Selección de entidades</li>
-                    <li>Administración financiera</li>
-                </ul>
-                <a href="#">MÁS</a>
-            </div>
-        </div>
+$app->run();
 
-
-        <div class="row">
-            <div class="col-md-4 post">
-                <h1>Noticias y Publicaciones</h1>
-                <img src="/img/post_1.jpg" />
-                <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
-                <a href="#">Más...</a>
-            </div>
-            <div class="col-md-4 post">
-                <img src="/img/post_2.jpg" />
-                <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
-                <a href="#">Más...</a>
-            </div>
-            <div class="col-md-3 post">
-                <h1>Testimonios</h1>
-                <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí puedes contar tu historia y permitir a tus usuarios saber más sobre ti.</p>
-                <h3>Samuel Díaz</h3>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="footer">
-        <div class="contact">
-
-        </div>
-        <div class="follow">
-
-        </div>
-        <div class="member">
-
-        </div>
-    </div>
-</body>
-
-</html>
+?>
